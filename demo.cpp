f0a1c5ba1748278fdfa645f00c5a2dbce1cf2c07
Lindsey Goode <ljgoode2012@gmail.com>
2026-02-03 11:43:09 -0600

    integrated qt library

--- demo.cpp
+++ demo.cpp
@@ -1,29 +1,106 @@
 ﻿#include <iostream>
 
+#include <iostream>
 #include <opencv2/opencv.hpp>
 #include <opencv2/core.hpp>
+#include <QtWidgets>
+#include <QApplication>
+#include <QLabel>
+#include <QImage>
+#include <QTimer>
+#include <QRadioButton>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
 
 using namespace cv;
 using namespace std;
 
-int main() {
+class VideoWidget : public QWidget {
+    Q_OBJECT
+private:
+    QLabel *label;
+    VideoCapture camera;
+    QTimer *timer;
+    bool isBlackWhite = false;
 
-    VideoCapture cap(0);
-    if(!cap.isOpened()) {
-        cout << "Error opening video stream or file" << endl;
-        return -1;
+public:
+    VideoWidget() {
+        cout << "VideoWidget constructor starting..." << endl;
+        label = new QLabel();
+        camera.open(0);
+        
+        if (!camera.isOpened()) {
+            label->setText("Error: Cannot open camera!");
+            resize(400, 100);
+            return;
+        }
+        
+        timer = new QTimer(this);
+        connect(timer, &QTimer::timeout, this, &VideoWidget::updateFrame);
+        timer->start(30);  // Update every 30ms
+        
+        resize(800, 600);
+        setWindowTitle("Video Feed");
+
+        // Main layout
+        QVBoxLayout *mainLayout = new QVBoxLayout(this);
+        mainLayout->setContentsMargins(5, 5, 5, 5);
+        
+        QRadioButton *blackWhite = new QRadioButton("Black/White");
+        QRadioButton *colorMode = new QRadioButton("Color");
+
+        
+        connect(blackWhite, &QRadioButton::toggled, this, &VideoWidget::setBlackWhite);
+        mainLayout->addWidget(label);
+        mainLayout->addStretch();
+        mainLayout->addWidget(blackWhite);
+        mainLayout->addWidget(colorMode); 
+        
+        setLayout(mainLayout);
     }
-    Mat frame;
-    int key;
-    
 
-    while(true) {
-        cap >> frame;
-        imshow("Video Frame", frame);
-        key = waitKey(30);
+    private slots:
+    void updateFrame() {
+        Mat frame;
+        camera >> frame;
+        
+        if (frame.empty()) {
+            cout << "Frame is empty!" << endl;
+            label->setText("No camera feed");
+            return;
+        }
+        
+        if (isBlackWhite) {
+            applyBlackWhite(frame);
+        } else {
+            cvtColor(frame, frame, COLOR_BGR2RGB);
+        }
 
-        if (key == 27) {  // Exit on 'ESC' key
-            break;}
+        QImage image = QImage(frame.data, frame.cols, frame.rows, 
+                             frame.step, QImage::Format_RGB888);
+        label->setPixmap(QPixmap::fromImage(image).scaledToWidth(750, Qt::SmoothTransformation));
     }
+    void applyBlackWhite(Mat &frame) {
+        cvtColor(frame, frame, COLOR_BGR2GRAY);
+        cvtColor(frame, frame, COLOR_GRAY2RGB);
+    }
+    void setBlackWhite(bool checked) {
+        isBlackWhite = checked;
+    }
+};
 
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    VideoWidget window;
+    window.show();
+    return app.exec();
 }
+
+#include "demo.moc"
+
+
+
+
